c5032dac9537f123f576873c1cfb25845235c119
Tom Clarke <tom@u2i.com>
2024-07-30 16:15:33 -0400

    updating spark

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Teamology.MixProject do
   defp deps do
     [
       {:ash, "~> 3.3.0"},
+      {:spark, "~> 2.2.11"},
       {:phoenix, "~> 1.7.12"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
